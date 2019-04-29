4ce8a0c6be9c9f44b8f294d116b7254b71131773
J.Martensen <jule.m.martensen@student.fh-kiel.de>
2019-04-29 10:56:59 +0200

    add task solution

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const {GraphQLServer, withFilter, PubSub} = require('graphql-yoga');
 //consts
 
 const NEW_USER_CREATED = 'NEW_USER_CREATED';
+const ITEM_BOUGHT = 'ITEM_BOUGHT';
 
 //data
 
@@ -13,11 +14,26 @@ const users = [
   {
     name: 'Albert Einstein',
     age: 140,
+    budget: 5000,
+    items: [],
   },
   {
     name: 'Bibi Blocksberg',
     age: 39,
+    budget: 30,
+    items: [],
   },
+];
+
+const items = [
+  {
+    title: 'Book',
+    price: 10,
+  },
+  {
+    title: 'Coffee',
+    price: 1.50,
+  }
 ];
 
 //typeDefs
@@ -30,15 +46,24 @@ const typeDefs = `
   
   type Mutation{
    createUser(name: String!, age: Int!): User!
+   buy(userName: String!, itemName: String!): User!
   }
   
   type Subscription {
     newUser: User!
+    itemBought: String!
   }
   
   type User {
     name: String!
     age: Int!
+    budget: Float!
+    items: [Item]!
+  }
+  
+  type Item {
+    title: String!
+    price: Float! 
   }
 `;
 
@@ -55,19 +80,34 @@ const resolvers = {
       const newUser = {
         name: name,
         age: age,
-        alive: true
+        budget: 100,
+        items: [],
       };
       users.push(newUser);
       //publish to subscription, name of the payload must equal the name of subscription
       pubsub.publish(NEW_USER_CREATED, {newUser})
       return newUser;
     },
+    buy: (_, {userName, itemName}) => {
+      const item = items.find(item => item.title === itemName);
+      const user = users.find(user => user.name === userName);
+      user.items.push(item);
+      user.budget -= item.price;
+      const itemBought = `${user.name} bought a ${item.title} and has ${user.budget} left`;
+      pubsub.publish(ITEM_BOUGHT, {itemBought});
+      return user;
+    }
   },
 
   Subscription: {
     newUser: {
       subscribe: (root, args, {pubsub}) => {
         return pubsub.asyncIterator(NEW_USER_CREATED);
+      }
+    },
+    itemBought: {
+      subscribe: (root, args, {pubsub}) => {
+        return pubsub.asyncIterator(ITEM_BOUGHT);
       }
     }
   }
